8cc7536da6a5d4999f713923d2fdb405e846dd8f
AvasBrahma <avasbrahma@gmail.com>
2024-11-09 23:15:07 +0530

    added step def for button action and text field

--- stepDefinitions/commonUIStepDefinition.js
+++ stepDefinitions/commonUIStepDefinition.js
@@ -13,5 +13,15 @@ Given('Navigate to the Website', async function(){
 })
 
 When('Click the button {string} in the {string} page', async function(buttonName, pageName){
-  
+   const pageFactory=new PageFactory();
+   const specificPage=pageFactory.getPageObject(pageName);
+   let commonActions=new CommonActions(page);
+   await commonActions.clickButton(specificPage, buttonName);
+});
+
+When('Enter the input text for {string} as {string} in the {string} Page', async function(inputFieldName, inputFieldValue, pageName){
+  const pageFactory=new PageFactory();
+  const specificPage=pageFactory.getPageObject(pageName);
+  let commonActions=new CommonActions(page);
+  await commonActions.enterText(specificPage, inputFieldValue, inputFieldName);
 })
